70bdc657745917cf68452bb169d0970c4e71135b
alfredo-capitia <delciiocapitia676@gmail.com>
2025-06-03 10:22:15 +0100

    feat: criando um novo componente pra poder visualizar os loquidos num recipiente

--- src/components/resultado.tsx
+++ src/components/resultado.tsx
@@ -1,21 +1,21 @@
-
 type ResultadoProps = {
-    resultado: { pressaoTotal: number; detalhes: string[] };
-
-}
+  resultado: { pressaoTotal: number; detalhes: string[] };
+};
 
 export function Resultado({ resultado }: ResultadoProps) {
-    return (
-        <>
-            <div className="mt-6 p-4 bg-gray-500 rounded shadow">
-                <h2 className="font-bold mb-2">Resultado</h2>
-                <ul className="list-disc list-inside">
-                    {resultado.detalhes.map((d, i) => (
-                        <li key={i}>{d}</li>
-                    ))}
-                </ul>
-                <p className="mt-4 font-bold">Pressão total: {resultado.pressaoTotal.toFixed(2)} Pa</p>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="mt-6 p-4 bg-gray-500 rounded shadow">
+        <h2 className="font-bold mb-2">Resultado</h2>
+        <ul className="list-disc list-inside">
+          {resultado.detalhes.map((d, i) => (
+            <li key={i}>{d}</li>
+          ))}
+        </ul>
+        <p className="mt-4 font-bold">
+          Pressão total: {resultado.pressaoTotal.toFixed(2)} Pa
+        </p>
+      </div>
+    </>
+  );
 }
